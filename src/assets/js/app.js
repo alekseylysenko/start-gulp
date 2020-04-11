ae9f66589e226ac7777e3a336f07bb6f6705f274
Aleksey <lysenko-a@mail.com>
2020-04-11 19:12:45 +0300

    Загрузка проекта

---
+++ src/assets/js/app.js
@@ -0,0 +1,6 @@
+//= components/tabs.js
+//= components/modal.js
+//= components/slick.js
+//= components/header.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
